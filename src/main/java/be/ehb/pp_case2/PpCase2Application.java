6168c35efebff43b75dadef3f25e1088463d3743
RaihaneBenjilali <raihane.benjilali@student.odisee.be>
2025-06-02 14:14:27 +0200

    Begin

---
+++ src/main/java/be/ehb/pp_case2/PpCase2Application.java
@@ -0,0 +1,13 @@
+package be.ehb.pp_case2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpCase2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpCase2Application.class, args);
+    }
+
+}
---
+++ src/test/java/be/ehb/pp_case2/PpCase2ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.pp_case2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpCase2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
